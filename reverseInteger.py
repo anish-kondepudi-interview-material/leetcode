878ff929acf93533818d09f0f52b1d1d24c0a5f2
Anish Kondepudi <anish.kondepudi@gmail.com>
2021-08-11 20:22:59 -0700

    Updated Code

--- reverseInteger.py
+++ reverseInteger.py
@@ -1,4 +1,22 @@
 class Solution:
+class Solution:
     def reverse(self, x: int) -> int:
-        reverse = int(str(x)[::-1]) if x>=0 else int('-'+str(x)[1:][::-1])
-        return 0 if (reverse > 2**31 -1 or reverse < -2**31) else reverse
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+        
+        reverseNum = 0
+        
+        isPositive = True if x >= 0 else False
+
+        if not isPositive:
+            x *= -1
+            reverseNum = (x%10) * -1
+            x //= 10
+
+        while x != 0:
+            if reverseNum > INT_MAX//10 or reverseNum < INT_MIN/10:
+                return 0
+            reverseNum = (reverseNum*10 + x%10) if isPositive else (reverseNum*10 - x%10)
+            x //= 10
+        
+        return reverseNum
