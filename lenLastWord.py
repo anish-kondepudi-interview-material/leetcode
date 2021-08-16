6e776be0ef7e98b35f1bdd85ba0d10f673cbae90
anish-kondepudi <anish.kondepudi@gmail.com>
2021-08-16 14:29:29 -0700

    lenLastWord (Alternate Solution)

--- lenLastWord.py
+++ lenLastWord.py
@@ -1,4 +1,21 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(s.split()[-1])
+        
+        # In Built functions
+        # return len(s.split()[-1])
+        
+        # No in-built functions
+        lenWord = 0
+        charIdx = len(s)-1
+        
+        while (s[charIdx] == " "): charIdx -= 1
+        
+        while (charIdx >= 0):
+            if s[charIdx] == " ":
+                return lenWord
+            lenWord += 1
+            charIdx -= 1
+
+        return lenWord
+    
         
