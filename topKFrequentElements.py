bac0b63b8f824cac2533544d88ce39e8684220e4
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-02-01 21:10:21 -0800

    347. Top K Frequent Elements - Added O(N) solution

--- topKFrequentElements.py
+++ topKFrequentElements.py
@@ -16,3 +16,26 @@ class Solution:
             kMostFreqEl.append(heappop(heap)[1])
             
         return kMostFreqEl
+
+# O(N) Solution
+# from collections import defaultdict
+
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+#         occurrences = defaultdict(int)
+#         for num in nums:
+#             occurrences[num] += 1
+
+#         countArr = [[] for _ in range(len(nums)+1)]
+#         for num,count in occurrences.items():
+#             countArr[count].append(num)
+
+#         topKFreqEl = []
+#         for arr in reversed(countArr):
+#             if k == 0: break
+#             while len(arr) != 0:
+#                 topKFreqEl.append(arr.pop())
+#                 k -= 1
+
+#         return topKFreqEl
